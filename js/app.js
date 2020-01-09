a04bd82b87c249e31fcc50597f5312c6e8cb1c8e
dirrbick <kory.a.jaxn@gmail.com>
2020-01-08 17:52:01 -0800

    fixed where the images get rendered to

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 'use strict';
-
-let keywordsArr = ['narwhal', 'rhino', 'unicorn', 'unilego', 'triceratops', 'markhor', 'mouflon', 'addax', 'chameleon', 'lizard', 'dragon'];
+const keywordsArr = [];
 
 function Horns(item) {
   this.url = item.image_url;
@@ -8,23 +7,24 @@ function Horns(item) {
   this.desc = item.description;
   this.keyword = item.keyword;
   this.numhorns = item.horns;
+
+  keywordsArr.push(this.keyword);
 }
 
 Horns.allHorns = [];
 
 Horns.prototype.render = function() {
-  $('main').append('<div class="copy"></div>');
-  let $hornsCopy = $('div[class = "copy"]');
+  $('main').append('<section class="copy"></section>');
+  let $hornsCopy = $('section[class = "copy"]');
   let $hornsHTML = $('#photo-template').html();
 
   $hornsCopy.html($hornsHTML);
-  console.log($hornsHTML);
   $hornsCopy.find('h2').text(this.title);
   $hornsCopy.find('img').attr('src', this.url);
   $hornsCopy.find('p').text(this.desc);
   $hornsCopy.removeClass('copy');
   $hornsCopy.attr('class', this.title);
-  console.log('after find and text', $hornsHTML);
+
 };
 
 Horns.readJson = () => {
@@ -36,7 +36,6 @@ Horns.readJson = () => {
     })
     .then(Horns.loadHorns);
 };
-
 Horns.loadHorns = () => {
   Horns.allHorns.forEach(Horns => Horns.render());
 };
@@ -44,14 +43,15 @@ Horns.loadHorns = () => {
 $(() => Horns.readJson());
 
 // Create a <select> element which contains unique <option> elements extracted dynamically from the JSON file, one for each keyword.
+console.log(keywordsArr);
+const optionsRender = (arr) => {
+  arr.forEach((keySelect) => {
+    $('select').append(`<option>${arr}</option>`);
+  });
+};
+console.log('options function', $(() => optionsRender(keywordsArr)));
 
-keywordsArr.forEach(function(keySelect) {
-  let keyOption = keySelect;
-  $('select').append(`<option>${keyOption}</option>`);
-});
-
-$(() => keywordsArr.forEach());
-
+$(() => optionsRender(keywordsArr));
 // let selectKey = () => {
 // $('option').on('select', keywordsArr.forEach());
 
